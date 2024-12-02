a71afb114e05819dbab36f00fcc44bb702bde375
Andrew Brown <andybrown213@yahoo.com>
2024-12-02 13:05:39 -0600

    adjusted favicon to the record, got rid of uneccessary cra files

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 function Header() {
 
-    /* const randomStringGenerator = (length) => {
+    const randomStringGenerator = (length) => {
         const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-.~';
         const values = crypto.getRandomValues(new Uint8Array(length));
         return values.reduce((acc, x) => acc + possible[x % possible.length], "");
     }
     
     const codeVerifier = randomStringGenerator(128);
+
+    /*
     
     const sha256 = async (plain) => {
         const encoder = new TextEncoder();
@@ -36,6 +38,8 @@ function Header() {
 
         <h1>Are you ready to Jammmm?!</h1>
         <button>Connect Your Spotify</button>
+        <p>{crypto.getRandomValues(new Uint8Array(128))}</p>
+        <p>{codeVerifier}</p>
 
         </header>
     );
