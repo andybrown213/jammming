8eb641aca44e9513c125064cba46b957d380befb
Andrew Brown <andybrown213@yahoo.com>
2024-12-06 16:45:58 -0600

    scratch that, doing all auth in auth window

--- src/Auth.js
+++ src/Auth.js
@@ -1,66 +1,110 @@
 //import React from 'react';
 
+const clientId = 'a1eeb89897404526bb54efd92df7a6f2';
+const redirectUri = 'https://gorgeous-bombolone-0ba30e.netlify.app/auth';
+
+function getCode () {
+    
+    const randomStringGenerator = (length) => {
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-.~';
+        const values = crypto.getRandomValues(new Uint8Array(length));
+        return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+    }
+
+    function challengeCreator(codeVerifier) {
+    
+        const sha256 = (plain) => {
+            const encoder = new TextEncoder();
+            const data = encoder.encode(plain);
+            return window.crypto.subtle.digest('SHA-256', data);
+        }
+        
+        const base64encode = (input) => {
+            return btoa(String.fromCharCode(...new Uint8Array(input)))
+              .replace(/=/g, '')
+              .replace(/\+/g, "-")
+              .replace(/\//g, '_');
+        }
+        
+        const hashed = sha256(codeVerifier);
+        const codeChallenge = base64encode(hashed);
+        
+        return codeChallenge;
+    }
+    
+    const codeVerifier = randomStringGenerator(128);
+    const scope = 'user-modify-playback-state user-read-playback-state user-read-currently-playing playlist-modify-private playlist-modify-public user-library-read user-library-modify user-read-playback-position';
+    const authUrl = new URL('https://accounts.spotify.com/authorize');
+
+    const codeChallenge = challengeCreator(codeVerifier);
+    localStorage.setItem('code verifier', codeVerifier);
+
+    const params = {
+        response_type: 'code',
+        client_id: clientId,
+        scope,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge,
+        redirect_uri: redirectUri
+    }
+
+    authUrl.search = new URLSearchParams(params).toString();
+    document.location.href = authUrl.toString();
+}
+
+async function getToken(code) {
+
+    const url = 'https://accounts.spotify.com/api/token';
+    const codeVerifier = localStorage.getItem('code verifier');
+
+    const payload = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier
+    })
+    }
+
+    let response, json;
+
+    try {
+        response = await fetch(url, payload);
+        json = await response.json();
+    } catch (error) {
+        console.log(`Fetch Error: ${error}`);
+    }
+    
+    console.log(response);  
+    console.log(json);
+
+    if (!response.ok) {
+            const error = `We have encountered an error from Spotify. Status Code: ${response.status} Error: ${json.error} Details: ${json.error_description}`;
+            document.innerHTML = error;
+    }  
+
+    localStorage.setItem('access_token', json.access_token);
+
+    document.innerHTML = 'Login Successful! Good Job!';
+
+}
+
 
 function Auth() {
     
-    let codeVerifier;
-    
-    window.addEventListener(
-        'message',
-        (event) => {
-            if ((event.origin !== 'https://gorgeous-bombolone-0ba30e.netlify.app') || (typeof event.data !== 'string')) return;
-            codeVerifier = event.data;
-            console.log('verifier received')
-        }
-    )
+    if (window.location.search === '') getCode();
 
     const urlParams = new URLSearchParams(window.location.search);
-
     let code = urlParams.get('code');
     let error = urlParams.get('error');
 
-    async function getToken(code) {
-
-        const clientId = 'a1eeb89897404526bb54efd92df7a6f2';
-        const redirectUri = 'https://gorgeous-bombolone-0ba30e.netlify.app/auth';
-        const url = 'https://accounts.spotify.com/api/token';
-
-        const payload = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-            client_id: clientId,
-            grant_type: 'authorization_code',
-            code,
-            redirect_uri: redirectUri,
-            code_verifier: codeVerifier
-        })
-        }
-
-        let response, json;
-
-        try {
-            response = await fetch(url, payload);
-            json = await response.json();
-        } catch (error) {
-            console.log(`Fetch Error: ${error}`);
-        }
-        
-        console.log(response);  
-        console.log(json);
-    
-        if (!response.ok) {
-            throw new Error(`We have encountered an error from Spotify. Status Code: ${response.status} Error: ${json.error} Details: ${json.error_description}`);
-        }   
-
-        localStorage.setItem('access_token', json.access_token);
-        document.innerHTML = 'Login Successful! Good Job!';
-
-    }
-
     error === null ? getToken(code) : document.innerHTML = error;
+
 }
 
 export default Auth;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,13 @@
 import React from 'react';
 
-function challengeCreator(codeVerifier) {
-    
-    const sha256 = (plain) => {
-        const encoder = new TextEncoder();
-        const data = encoder.encode(plain);
-        return window.crypto.subtle.digest('SHA-256', data);
-    }
-    
-    const base64encode = (input) => {
-        return btoa(String.fromCharCode(...new Uint8Array(input)))
-          .replace(/=/g, '')
-          .replace(/\+/g, "-")
-          .replace(/\//g, '_');
-    }
-    
-    const hashed = sha256(codeVerifier);
-    const codeChallenge = base64encode(hashed);
-    
-    return codeChallenge;
-}
+
 
 function handleLogin() {
 
-    const randomStringGenerator = (length) => {
-        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-.~';
-        const values = crypto.getRandomValues(new Uint8Array(length));
-        return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-    }
-    
-    const codeVerifier = randomStringGenerator(128);
-    const clientId = 'a1eeb89897404526bb54efd92df7a6f2';
-    const redirectUri = 'https://gorgeous-bombolone-0ba30e.netlify.app/auth';
-    const scope = 'user-modify-playback-state user-read-playback-state user-read-currently-playing playlist-modify-private playlist-modify-public user-library-read user-library-modify user-read-playback-position';
-    const authUrl = new URL('https://accounts.spotify.com/authorize');
 
-    const codeChallenge = challengeCreator(codeVerifier);
+    window.open('https://gorgeous-bombolone-0ba30e.netlify.app/auth', 'Spotify Authorization Window', 'popup, width=500, height=800');
 
-    const params = {
-        response_type: 'code',
-        client_id: clientId,
-        scope,
-        code_challenge_method: 'S256',
-        code_challenge: codeChallenge,
-        redirect_uri: redirectUri
-    }
 
-    authUrl.search = new URLSearchParams(params).toString();
-    const authWindow = window.open(authUrl.toString(), 'Spotify Authorization Window', 'popup, width=500, height=800');
-
-    authWindow.postMessage(codeVerifier, 'https://gorgeous-bombolone-0ba30e.netlify.app');
 }
 
 function Login() {
