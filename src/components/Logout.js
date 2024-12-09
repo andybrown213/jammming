06253610396c60e6dbf3b716136a1f013e41aeed
Andrew Brown <andybrown213@yahoo.com>
2024-12-09 06:45:55 -0600

    finaly trying to display actual profile info

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -9,29 +9,25 @@ function Logout() {
 
     const accessToken = localStorage.getItem('access token');
 
+    let userProfile;
+
     async function getProfile(accessToken) {
 
         const result = await fetch('https://api.spotify.com/v1/me', {
             method: 'get', headers: {Authorization: `Bearer ${accessToken}`}           
         });
 
-        const json = await result.json();
-
-        console.log(result);
-        console.log(json);
-        
-
-        return json;
+        userProfile = await result.json();
 
     }
 
-    const profile = getProfile(accessToken).then(((response) => {return response}));
+    getProfile(accessToken);
 
-    console.log(profile)
+    console.log(userProfile);
 
     return (
         <div id='logout'>
-        <h2>Welcome, {profile.display_name}!</h2>
+        <h2>Welcome, {userProfile.display_name}!</h2>
         <button onClick={handleLogout}>Logout</button>
         </div>   
     )
