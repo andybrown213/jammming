24fb8dc221e97027bc3b1c5a60e036c84e658680
Andrew Brown <andybrown213@yahoo.com>
2024-12-09 16:07:42 -0600

    Added user playlist fetch function to App comp and created UserPlaylists component

--- src/components/UserPlaylists.js
+++ src/components/UserPlaylists.js
@@ -5,13 +5,16 @@ function UserPlaylists (props) {
     console.log(props.userPlaylists);
     
     return (
-
         <div className='user-playlists'>
 
-            
+            {props.userPlaylists.map.items((item) => {
+                <div id='playlist'>
+                    <h3>{item.id}</h3>
+                    <h4>Owner: {item.owner}</h4>
+                </div>
+            })}
 
         </div>
-
     )
 }
 
