97eadf04fc0eb357f3ee779444d02e00043f8bd8
Andrew Brown <andybrown213@yahoo.com>
2024-12-09 13:15:19 -0600

    added profile picture display, made login logout buttons smaller

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function Header(props) {
     let profilePicture, profileName;
 
     if ((props.userProfile) && (props.loggedIn)) {
-        profilePicture = props.userProfile.images[0];
+        profilePicture = props.userProfile.images[0]['url'];
         console.log(props.userProfile);
         profileName = props.userProfile.display_name;
     }
