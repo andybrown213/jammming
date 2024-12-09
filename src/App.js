5194d0053999986c33c01b4564ac067fe881b35c
Andrew Brown <andybrown213@yahoo.com>
2024-12-09 12:03:39 -0600

    got rid of login and logout components and moved into header. moved profile fetch to main app component and passed state down to header.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
 import RecordPlayer from './components/RecordPlayer'
 import './App.css';
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   
   const [loggedIn, setLoggedIn] = useState(false);
+  const [userProfile, setUserProfile] = useState();
 
   const checkAccess = () => {
     let accessToken = localStorage.getItem('access token');
@@ -14,10 +15,43 @@ function App() {
 
   if (checkAccess() !== loggedIn) {setLoggedIn(checkAccess)};
 
+  async function populateUI () {
+
+    const accessToken = localStorage.getItem('access token');
+
+    let userProfile;
+
+    async function getProfile(accessToken) {
+
+        let json;
+
+        try {
+            const response = await fetch('https://api.spotify.com/v1/me', {
+                method: 'get', headers: {Authorization: `Bearer ${accessToken}`}           
+            });
+            json = await response.json();         
+    
+            if (!response.ok) {
+                throw new Error(`status code: ${response.status} Error: ${json.error} Description: ${json.error_description}`)
+            }
+        } catch (error) {console.log(error)};
+
+        return json;
+    }
+
+    if (!userProfile) {
+        getProfile(accessToken)
+          .then(response => setUserProfile(response))
+          .catch(error => console.log(`Error fetching user data: ${error}`));
+    }
+  }
+
+  useEffect(() => {populateUI()}, [loggedIn]);
+
   return (
     <div className="app">
 
-        <Header loggedIn={loggedIn} />
+        <Header loggedIn={loggedIn} userProfile={userProfile} />
 
         <div className="left-navigation">
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import Login from './Login';
-import Logout from './Logout';
+
+function handleLogout() {
+    window.localStorage.clear();
+    window.location.reload();
+}
+
+function handleLogin() {
+    window.open('https://gorgeous-bombolone-0ba30e.netlify.app/auth', 'Spotify Authorization Window', 'popup, width=500, height=800');
+}
 
 function Header(props) {
     
     if (props.loggedIn === true) {
         return (
             <header>
-                <Logout />
+                <h2>Welcome{props.userProfile ? `, ${props.userProfile.display_name}` : ''}</h2>
+                <button onClick={handleLogout}>Logout</button>
             </header>
         )
     } else {
         return (
             <header>
-                <Login />
+                <h1>Are you ready to Jammm?!</h1>
+                <button onClick={handleLogin}>Connect Your Spotify</button>  
             </header>
         )
     }
-
 }
 
 export default Header;
