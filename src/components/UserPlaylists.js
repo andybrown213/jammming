f6b69212c0566cdbb08c31386836e0060224f998
Andrew Brown <andybrown213@yahoo.com>
2024-12-14 14:01:13 -0600

    adding functionality to main play button

--- src/components/UserPlaylists.js
+++ src/components/UserPlaylists.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import playHandler from './PlayerInterface';
+import { playHandler } from './PlayerInterface';
 
 
 function UserPlaylists (props) {
@@ -15,7 +15,7 @@ function UserPlaylists (props) {
                     <div className='playlist'>
                         <h4>{item.name ? item.name : 'No Name Playlist..'}</h4>
                         <h5>owner: {item.owner.display_name}</h5>
-                        <button onClick={(item) => playHandler(item.uri)}>Play</button>
+                        <button onClick={() => playHandler(item.uri)}>Play</button>
                     </div>
                 ))}
     
