65ff6d9ee9f727a817cf1674aa3e06601e93228c
Andrew Brown <andybrown213@yahoo.com>
2024-12-17 09:51:11 -0600

    sorted recently played the right direction and put queue and recently played updater fucntions after await in interface sync to give a second for recently played song to update in server while state is requested

--- src/components/UserQueue.js
+++ src/components/UserQueue.js
@@ -11,7 +11,7 @@ export default function UserQueue (props) {
 
         if (props.recentlyPlayed) {
             console.log(props.recentlyPlayed);
-            setRecentSongs(props.recentlyPlayed.items.toSorted((a, b) => b.played_at - a.played_at));
+            setRecentSongs(props.recentlyPlayed.items.toSorted((b, a) => a.played_at - b.played_at));
         } else {
             setRecentSongs([{track: {name: 'Nothing Played Recently', artists: [{name: ''}]}}]);
         }
